1b7ee1c606cc9e11e35c90a0a0a8fcca5ae5e3a0
TheingeYadanarHtun <64574546+theinge-yadanar-htun@users.noreply.github.com>
2021-06-26 22:43:48 +0630

    Update admin_panel.js

--- JS/admin_panel.js
+++ JS/admin_panel.js
@@ -1,1 +1,26 @@
+/* 2.Logout
+ *2.1 Destroy local storage data
+ *2.2 Go to login page
+ */
+
+
+function clickMe() {
+    console.log("user click me");
+    //Destroy local storage data
+    window.localStorage.removeItem('email');
+    window.localStorage.removeItem('password');
+    window.localStorage.clear();
+    //Go to login page
+    window.location.href = "login.html";
+
+}
+
+
+
+
+
+console.log("Destroy Local Storage Data");
+console.log("Login Page");
+
+
 
